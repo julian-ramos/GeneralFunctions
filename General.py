3be47650e9d5293936fd5cd5ef587845602387c6
Julian Ramos <ing.julianr@gmail.com>
2013-07-22 18:50:54 -0400

    Added the paths for the Discovering_APPC project

--- General.py
+++ General.py
@@ -43,6 +43,24 @@ def projectPaths(projectName):
             print 'Moved to the data path '+data_path
             os.chdir(data_path);
         return {'main_path':main_path,'data_path':data_path,'results_path':results_path}
+    
+    if projectName == 'Discovering_APPC':
+        if A=='JULIAN':
+            print 'working on JULIAN@CMU'
+            print 'This paths have not being defined yet'
+            main_path='F:/Activity_RS/Dataset'
+            data_path='F:/Activity_RS/Dataset'
+            results_path='F:/Activity_RS/Results'
+            print 'Moved to the data path '+data_path
+            os.chdir(data_path);
+        else :
+            print 'working on Julian@laptop'
+            main_path='E:\research_data_sets\neill\upmc'
+            data_path='E:\research_data_sets\neill\upmc\DeIDOutput\copd_pat_10'
+            results_path='--not defined yet--'
+            print 'Moved to the data path '+data_path
+            os.chdir(data_path);
+        return {'main_path':main_path,'data_path':data_path,'results_path':results_path}
             
     
     
